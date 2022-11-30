16880fa0678534ca695d878c7bb7872640755534
tatialveso <tatiialveso@gmail.com>
2022-11-30 14:09:49 -0300

    :sparkles: iteration 3 done

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import Recipe from "../models/Recipe.model.js";
+import data from '../data.json' assert { type: 'json' }
 
 const router = express.Router()
 
@@ -9,6 +10,18 @@ router.post('/create', async (req, res) => {
         
         
         return res.status(201).json(newRecipe)
+    } catch (error) {
+        console.log(error)
+
+        return res.status(500).json({ msg: 'Sorry, something is wrong. Try again later!'})
+    }
+})
+
+router.post('/insert', async (req, res) => {
+    try {
+        const insertRecipes = await Recipe.insertMany(data)
+
+        return res.status(201).json(insertRecipes)
     } catch (error) {
         console.log(error)
 
