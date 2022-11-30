46748214e85c00ee7d86f03381142cf9769272b3
tatialveso <tatiialveso@gmail.com>
2022-11-30 11:49:19 -0300

    :sparkles: iteration 1 done

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,41 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+import mongoose, { Schema } from 'mongoose'
 
-const recipeSchema = new Schema({
-  // TODO: write the schema
-});
+const recipeSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    level: {
+      type: String,
+      enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+    },
+    ingredients: [String],
+    cuisine: {
+      type: String,
+      required: true
+    },
+    dishType: {
+      type: String
+    },
+    image: {
+      type: String,
+      default: "https://images.media-allrecipes.com/images/75131.jpg"
+    },
+    duration: {
+      type: Number,
+      min: 0
+    },
+    creator: {
+      type: String
+    }
+  },
+  {
+    timestamps: true
+  }
+);
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
-module.exports = Recipe;
+export default Recipe
